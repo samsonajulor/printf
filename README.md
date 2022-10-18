5ae109c703e4d224f61b3982d2452180782326d3
samsonajulor <s.ajulor@abbeymortgagebank.com>
2022-10-18 21:54:09 +0100

    added new files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-**This is a Printf group project done by Abenezer Zelalem and Victor Kenneth**
+**Creating our printf function**
+============================
+- This repository includes our version of the printf function, the basic and main functions.
+Here we will apply the knowledge that we have acquired during the learning of the programming language C.
+- This project is requested by Holberton School.
+----------------------------
+**Task**
+======
+
+0. Write a function that produces output according to a format.
+- c
+- s
+- %
+
+1. Handle the following conversion specifiers:
+- d
+- i
+
+2. Create a man page for your function.
+-----------------------------
+**Authors**
+=========
+- [Jose Marulanda](https://github.com/JoseMarulanda).
+- [Marcela Sanchez](https://github.com/marcewp15).
--- _printf.c
+++ _printf.c
@@ -1,50 +1,52 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
-
+#include <stddef.h>
 /**
- * _printf - a function that produces output according to a format
- *
- * @format: a pointer to the format string
- *
- * Return: on success, returns the number of characters printed
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
-
-	va_start(ap, format);
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
+	if (format != NULL)
 	{
-		init_params(&params, ap);
-		if (*p != '%')
+		int count = 0, i;
+		int (*m)(va_list);
+		va_list args;
+
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1] == '\0')
+			return (-1);
+		while (format != NULL && format[i] != '\0')
 		{
-			sum += _putchar(*p);
-			continue;
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					m = get_func(format[i + 1]);
+					if (m)
+						count += m(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
+			}
+			else
+			{
+				count += _putchar(format[i]);
+				i++;
+			}
 		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-					params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
+		va_end(args);
+		return (count);
 	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+	return (-1);
 }
